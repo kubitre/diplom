fff9e4ff16952f6c1cec0e2ea1fe948dacd32ffb
kubitre <kubitre@gmail.com>
2020-04-12 19:56:14 +0700

    upgrade

--- startinfra.sh
+++ startinfra.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --name=dev-consul --net=host -e CONSUL_BIND_INTERFACE=eno2 -p 8500:8500  consul
+docker run -d --name=dev-consul --net=host -e CONSUL_BIND_INTERFACE=eno2 -e CONSUL_HTTP_AUTH=kubitre:password -p 8500:8500  consul
